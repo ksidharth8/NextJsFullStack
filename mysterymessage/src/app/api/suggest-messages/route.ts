442ec37a9b84e33cfee013748215959064037b65
ksidharth8 <btech10180.23@bitmesra.ac.in>
2024-12-27 21:48:09 +0530

    feat: implement generative AI model to suggest messages

--- mysterymessage/src/app/api/suggest-messages/route.ts
+++ mysterymessage/src/app/api/suggest-messages/route.ts
@@ -1,59 +1,56 @@
-import { streamText } from "ai";
-import { google as googleSDK } from "@ai-sdk/google";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
+// import required dependencies
 import {
 	GoogleGenerativeAI,
 	GoogleGenerativeAIResponseError,
 } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-// const google = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
-
-// const generationConfig = {
-// 	maxTokens: 100,
-// 	temperature: 0.5,
-// 	topP: 1,
-// 	frequencyPenalty: 0,
-// 	presencePenalty: 0,
-// 	stop: ["\n", "Human:"],
-// };
-
-// const model = google.getGenerativeModel({
-// 	model: "gemini-1.5-pro-latest",
-// 	generationConfig,
-// });
-
-// export async function POST(request: NextRequest) {
-// 	const { messages } = await request.json();
-// 	const prompt = messages[messages.length - 1].content;
-// 	const result = await model.generateContent(prompt);
-// 	return NextResponse.json(result.response.text(), { status: 200 });
-// }
-
-const google = createGoogleGenerativeAI({
-	apiKey: process.env.GOOGLE_API_KEY as string,
+// defive generative AI model and configuration
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY as string);
+const model = genAI.getGenerativeModel({
+	model: "gemini-1.5-pro-latest",
+	generationConfig: {
+		candidateCount: 1,
+		maxOutputTokens: 50,
+		temperature: 1.5,
+	},
 });
+export const runtime = "edge";	// define runtime
 
-const model = google("gemini-1.5-pro-latest");
-
-export const runtime = "edge";
-
-export async function POST(req: Request) {
+// define GET function
+export async function GET(req: Request) {
 	try {
-		const result = streamText({
-			model: model,
-			maxTokens: 400,
-			prompt:
-				"Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.",
-		});
+		// define prompt
+		const prompt =
+			"Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-		// example: use textStream as an async iterable
-		let text = "";
-		for await (const textPart of result.textStream) {
-			text += textPart;
-		}
+		// generate content
+		const result = await model.generateContent(prompt);
 
-		return text;
+		console.log(
+			"result.response.usageMetadata: ",
+			result.response.usageMetadata
+		);
+
+		// extract response
+		const response =
+			result.response.candidates &&
+			result.response.candidates[0]?.content.parts[0]?.text?.toString();
+
+		// sample response
+		// const response =
+		// 	"What's something you're learning or want to learn?||If you could travel anywhere in the world right now, where would you go and why?||What's a small act of kindness that you've either given or received that";
+
+		console.log("content from route.ts: ", response);
+
+		// return response as JSON
+		return Response.json(
+			{
+				success: true,
+				suggestedMessages: response,
+			},
+			{ status: 200 }
+		);
 	} catch (error) {
 		if (error instanceof GoogleGenerativeAIResponseError) {
 			// Google API error handling
